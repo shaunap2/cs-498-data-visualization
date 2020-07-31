27c220538766a904c4ec627e5ce0c201a1dfe78d
Shaun Phillips <68923930+shaunap2@users.noreply.github.com>
2020-07-31 17:00:24 -0500

    Circles with transitions and mouse events.

--- fig1.js
+++ fig1.js
@@ -5,12 +5,24 @@ function bodyLoad() {
 
   var fig1 = d3.select("#fig1");
 
-  fig1.selectAll("circle")
+  var dots = fig1.selectAll("circle")
     .data(data)
     .enter()
     .append("circle")
+    .on("mouseover", function(d) {
+      d3.select(this)
+        .attr("fill", "orange");
+    })
+    .on("mouseout", function(d) {
+      d3.select(this)
+        .attr("fill", "black");
+    })
+    .transition()
+    .duration(2000)
+    .delay(1000)
     .attr("r", function(d) { return d; })
     .attr("cx", function(d, i) { return 10 * i; })
     .attr("cy", function(d, i) { return 20 * i; })
+
     ;
 }

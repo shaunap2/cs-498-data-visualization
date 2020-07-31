803aee38f23ba37e88d0226a61957252848d5c71
Shaun Phillips <68923930+shaunap2@users.noreply.github.com>
2020-07-31 17:38:27 -0500

    Getting transitions and tooltips to play well together.

--- fig1.js
+++ fig1.js
@@ -1,6 +1,6 @@
 function bodyLoad() {
   "use strict";
-  console.log("here!");
+  console.log("generating fig1");
   var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   var fig1 = d3.select("#fig1");
@@ -9,20 +9,23 @@ function bodyLoad() {
     .data(data)
     .enter()
     .append("circle")
-    .on("mouseover", function(d) {
-      d3.select(this)
-        .attr("fill", "orange");
-    })
-    .on("mouseout", function(d) {
-      d3.select(this)
-        .attr("fill", "black");
-    })
     .transition()
     .duration(2000)
-    .delay(1000)
     .attr("r", function(d) { return d; })
     .attr("cx", function(d, i) { return 10 * i; })
     .attr("cy", function(d, i) { return 20 * i; })
-
+    .selection()
+    .on("mouseover", function(d) {
+      d3.select(this).attr("fill", "orange");
+    })
+    .on("mouseout", function(d) {
+      d3.select(this).attr("fill", "black");
+    })
+    .append("title")
+    .text(function(d) {
+      return "The current value is " + d;
+    })
     ;
+
+
 }
